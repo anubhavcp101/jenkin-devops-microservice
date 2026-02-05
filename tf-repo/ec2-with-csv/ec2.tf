6feaaa2a0803ce53604e1195888ed6ddee4b7317
anubhavcp101 <125131631+anubhavcp101@users.noreply.github.com>
2026-02-05 09:00:26 +0530

    added ec2 files

---
+++ tf-repo/ec2-with-csv/ec2.tf
@@ -0,0 +1,114 @@
+#
+variable "common_tags" {
+  default = {
+    "environment" = "dev"
+    "role"        = "app"
+  }
+}
+#
+variable "name" {
+  default = "win00"
+}
+
+#
+variable "key_id" {
+  default = "arn:aws:kms:<ergion>:<account>:key/<key-id>"
+}
+
+variable "ec2_iam_profile" {
+  default = "ebs-kms-role"
+}
+
+variable "ec2_sg" {
+  default = ["sg-xxx"]
+}
+
+locals {
+  ec2s_csv = csvdecode(file("inst.csv"))
+  nics = { for ec2 in local.ec2s_csv : ec2.Name => {
+    Name = "${ec2.Name}"
+    private_ip = "${ec2.PrivateIP}"
+    subnet_id = "${ec2.SubnetId}"
+  }}
+  ec2s = { for ec2 in local.ec2s_csv : ec2.Name => {
+    ami = ec2.AMI
+    instance_type = ec2.InstanceType
+    az = ec2.AZ
+    root_volume_size = ec2.rootVolume
+    volume_type = ec2.VolumeType
+  }}
+
+  vols = flatten([ 
+    for ec2 in local.ec2s_csv : [
+      for ebs in split(",",ec2.EbsVolume) : {
+        instance = ec2.Name
+        size = element(split(";",ebs),0)
+        device = element(split(";",ebs),1)
+      }
+    ]
+  ])
+
+}
+
+resource "aws_network_interface" "nic" {
+  for_each = local.nics
+  subnet_id       = each.value.subnet_id
+  private_ips     = [each.value.private_ip]
+  security_groups = var.ec2_sg
+  tags = merge(var.common_tags, {
+    Name = "nic-${each.value.Name}"
+  }) 
+}
+
+resource "aws_instance" "ec2" {
+  for_each = local.ec2s
+  ami                  = each.value.ami
+  instance_type        = each.value.instance_type
+  availability_zone    = each.value.az
+  iam_instance_profile = var.ec2_iam_profile
+  #associate_public_ip_address = false
+  key_name = "win-00"
+
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = true
+    kms_key_id            = var.key_id
+    volume_size           = each.value.root_volume_size
+    volume_type           = each.value.volume_type
+    tags                  = merge(var.common_tags, { Name = "rootVol-${each.key}" })
+  }
+
+  primary_network_interface {
+    network_interface_id = aws_network_interface.nic["${each.key}"].id 
+  }
+
+
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  tags = merge(var.common_tags, {
+    Name = "${each.key}"
+  })
+}
+
+resource "aws_ebs_volume" "vol" {
+  for_each = { for vol in local.vols : "${vol.instance}-${vol.device}" => vol }
+  availability_zone = var.instance_az
+  size              = each.value.size
+  type = var.vol_type
+  
+  encrypted = true
+  kms_key_id = var.key_id
+
+  tags = merge(var.common_tags, {
+    Name = "ebsVol-${var.name}"
+  })
+}
+
+resource "aws_volume_attachment" "vol_att" {
+  for_each = { for vol in local.vols : "${vol.instance}-${vol.device}" => vol }
+  device_name = each.value.device
+  volume_id   = aws_ebs_volume.vol["${each.key}"].id
+  instance_id = aws_instance.ec2["${each.value.instance}"].id
+}
